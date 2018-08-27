15f5f2481ba290bcdb218b4e2e55c5995c3079e1
Dion Häfner <dimh@dhigroup.com>
2018-08-27 14:13:30 +0200

    wip

--- run_fmask.sh
+++ run_fmask.sh
@@ -3,22 +3,28 @@
 set -e
 shopt -s nullglob
 
-if [ $1 == "--help" ]; then
+if [ "$1" == "--help" ]; then
     echo "Usage: run_fmask.sh FMASK_OPTIONS"
     exit 0
 fi
 
 MCROOT=/usr/local/MATLAB/MATLAB_Runtime/v93
+INDIR=/mnt/input-dir
 WORKDIR=/work
+OUTDIR=/mnt/output-dir
 
 # ensure that workdir is clean
 rm -rf $WORKDIR
 mkdir -p $WORKDIR
 cd $WORKDIR
 
-for f in /mnt/input-dir/*; do
+ls -l $INDIR
+
+for f in $INDIR/*; do
     ln -s $(readlink -f $f) $WORKDIR/$(basename $f)
 done
+
+ls -l $WORKDIR
 
 # run fmask
 /usr/GERS/Fmask_4_0/application/run_Fmask_4_0.sh $MCROOT "$@"
